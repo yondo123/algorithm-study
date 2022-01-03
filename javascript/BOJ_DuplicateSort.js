2ee031041c334f7d91bc524216dc793960a41396
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-01-03 23:21:21 +0900

    중복빼고 정렬하기

--- javascript/BOJ_DuplicateSort.js
+++ javascript/BOJ_DuplicateSort.js
@@ -4,26 +4,17 @@
  * @desc level: S5
  * @desc url  : https://www.acmicpc.net/problem/10867
  */
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 // const input = fs.readFileSync("../temp.txt").toString().trim().split("\n");
-let answer = "";
+let answer = '';
 const inputArray = input.slice(1).map((item) => Number(item));
-const sortedArray = quickSort(inputArray);
-
-sortedArray.forEach((element, index) => {
-    answer += element;
-    if (sortedArray.length - 1 === index) {
-        console.log(answer);
-    }
-    answer += "\n";
-});
+console.log(quickSort(inputArray).join('\n'));
 
 function quickSort(array) {
     if (array.length <= 1) {
         return array;
     }
-
     //배열의 마지막 인덱스를 pivot으로 설정
     const pivot = array[array.length - 1];
     const leftArr = [];
@@ -32,10 +23,10 @@ function quickSort(array) {
     //복사한 배열을 loop -> pivot보다 작으면 leftArray, 크면 rightArray
     for (const item of array.slice(0, array.length - 1)) {
         if (item < pivot) {
+            leftArr.push(item);
         } else if (item > pivot) {
             rightArr.push(item);
         }
     }
-
     return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 }
