80ef6fb9ff01155a3cea8594b150fbb7905b345f
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-03-26 23:57:17 +0900

    k번째수 (풀이중)

--- basic/section-4/k-order.js
+++ basic/section-4/k-order.js
@@ -2,7 +2,7 @@
  * @desc problem : K번째 수
  * @desc site : Olympiad
  * @desc level: 3
- * @desc solution : 
+ * @desc solution :
  */
 
 /**
@@ -12,12 +12,51 @@
  */
 function solution(card, k) {
     let answer = 0;
-    const revolve = card.length - 2; //8
-    const sotred = card.sort(function (a, b) {
-        return b - a;
-    });
+    let slicePos = 0;
+    const select = 3;
+    const numberCase = card.length - (select - 1); //8
 
+    for (let i = numberCase; i > 0; i--) {
+        const loop = numberCase - i + 1;
+        let end = 0;
+        let start = 0;
+        for (let j = 0; j < loop; j++) {
+            end += numberCase - j;
+            if (j === loop) {
+                console.log('start', end);
+            }
+        }
+        console.log(end);
+        if (k <= end) {
+            slicePos = numberCase - i;
+            break;
+        }
+    }
+
+    const sotred = card
+        .sort(function (a, b) {
+            return b - a;
+        })
+        .slice(slicePos);
+
+    console.log(sotred);
+
+    // 8 * 1; 8
+    // 8 * 2 - 1; 15
+    // 8 * 3 - 3; 21
+    // 8 * 4 - 6 :
+
+    /**
+     * 1 ~ 8 : (8) * * 1 8 + 0
+     * 9 ~ 15 (7) 8 + 7
+     * 16 ~21 (6) 8 + 7 + 6
+     * 22 ~ 26 (5)
+     * 27  ~ 30 (4)
+     * 31 ~ 33 (3)
+     * 34 ~ 35 (2)
+     * 36 (1)
+     */
 }
 
-const answer = solution([13, 15, 34, 23, 45, 65, 33, 11, 26, 42], 3);
-console.log(answer); //3
+const answer = solution([13, 15, 34, 23, 45, 65, 33, 11, 26, 42], 10);
+// console.log(answer);
