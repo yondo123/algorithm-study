ca55e9965df53ffc9be18c60ed6aeaf4e5c563a8
SeungJin Choi <46988995+yondo123@users.noreply.github.com>
2022-08-31 16:50:27 +0900

    쇠막대기 풀이

--- basic/section-6/iron-stick.js
+++ basic/section-6/iron-stick.js
@@ -1,12 +1,8 @@
 /**
- * @desc problem : 쇠 막대기
+ * @desc problem : 쇠 막대기 자르기
  * @desc site : Olympiad
- * @desc level: 3
+ * @desc level: 4
  * @desc solution : stack
- */
-
-/**
- * 1. 열린지 확인한다.
  */
 
 /**
@@ -16,34 +12,41 @@
 function solution(stick) {
     const open = '(';
     const close = ')';
+
     let stack = [];
     let answer = 0;
-    let isOpen = false;
+    let laserCheck = false;
+    let laserCount = 0;
 
     stick.split('').forEach(function (item, index) {
-        if (item === open) {
-            isOpen = true;
-            stack.push(item);
+        if (item === open || laserCheck) {
+            //막대기를 자른 후 여는 괄호라면, 막대기 stack을 증가
+            if (item === open) {
+                stack.push(item);
+                laserCheck = false;
+            } else {
+                //닫는 괄호라면 막대기 1개를 닫힘 처리 (1증가)
+                answer++;
+                stack.pop();
+            }
         } else {
+            const lastItem = stack.pop();
+            // 이전 괄호가 열려있을 때 바로 막대기를 자른다.
+            if (lastItem === open) {
+                laserCount++;
+                laserCheck = true;
+                //막대기를 자를 때 기준은 쌓인 stack 기준 (왼쪽)
+                answer += stack.length;
+            } else {
+                //잔여 막대기 증가
+                answer++;
+                stack.pop();
+            }
         }
-
-        // if (item === open) {
-        //     stack.push(item);
-        // } else {
-        //     const lastItem = stack.pop();
-        //     if (lastItem === open) {
-        //         console.log(lastItem, item + 'Whoops!! sticks : ' + stack.length);
-        //         answer += stack.length;
-        //     } else {
-        //         answer += 1;
-        //         stack.push(lastItem);
-        //         stack.push(item);
-        //     }
-        // }
     });
 
     return answer;
 }
-
-const answer = solution('(((()(()()))(())()))(()())');
-console.log(answer);
+const answer = solution('()(((()())(())()))(())');
+// const answer = solution('(((()(()()))(())()))(()())');
+console.log(answer); //17
